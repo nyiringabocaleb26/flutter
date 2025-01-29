9423c1b987091edbca1bfcf627398807be4e1127
nyiringabocaleb26 <132354169+nyiringabocaleb26@users.noreply.github.com>
2025-01-29 16:37:23 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Center(
+      child: Text("NYIRINGABO Caleb",style: TextStyle(color: Colors.black)),
+    ),
+  ));
+}
